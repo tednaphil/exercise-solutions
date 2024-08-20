bedbc1bc257452fed286f96d7e6ca6d0d889cc57
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-08-19 21:18:27 -0500

    Add paliSum solution and test

--- javascript/index.js
+++ javascript/index.js
@@ -66,7 +66,53 @@ function moveRobot (directionString) {
     return (currentPosition[0] === startingPosition[0] && currentPosition[1] === startingPosition[1])
   }
 
+// PALISUM PROMPT
+// Write a method/function that starts at 0 and finds the first twenty-five numbers where the number plus its inverse equals a palindrome that is greater than 1,000.
+
+// 47(number) + 74(inverse) = 121(palindrome) Note: This does not meet the greater than 1,000 rule.
+
+// Collect the twenty-five numbers in an array as the return value. Be sure to collect the number and not the sum.
+
+//PSEUDOCODE:
+//output: array of 25 numbers that when added to their inverse produce a palindrome greater 1000
+
+//create an empty array to store paliNums
+//define starting number (0) and assign as default value for num argument
+//increment starting number +1
+
+// convert number to string, reverse, convert back to integer
+// add starting number to converted & reversed number - store sum
+//evaluate sum against converted, reversed, converted sum
+//if that's a palindrome && > 1000 && paliSum array.length < 25, paliSum array. push startingNumber
+
+// if paliSum.length === 25, return paliSum array
+
+//Refactor notes
+//create converter function??
+//use a while loop??
+
+function paliSum(num = 0, arr = []) {
+  let startingNum = num;
+  const paliNums = arr;
+  let reverseNum = parseInt(
+    startingNum.toString().split("").reverse().join(""),
+  );
+  let sum = startingNum + reverseNum;
+  let isPalindrome =
+    sum === parseInt(sum.toString().split("").reverse().join(""));
+
+  if (paliNums.length === 25) {
+    return paliNums;
+  } else if (isPalindrome && sum > 1000 && paliNums.length < 25) {
+    paliNums.push(startingNum);
+    return paliSum((startingNum += 1), paliNums);
+  } else {
+    return paliSum((startingNum += 1), paliNums);
+  }
+}
+
 
 module.exports = {
-  moveRobot
+  moveRobot,
+  paliSum
 }
--- test/js-exercises-test.js
+++ test/js-exercises-test.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const { moveRobot } = require('../javascript/index')
+const { moveRobot, paliSum } = require('../javascript/index')
 
 describe('JavaScript Exercises', () => {
     describe('MoveRobot', () => {
@@ -10,6 +10,18 @@ describe('JavaScript Exercises', () => {
         })
         it('ends at starting position', () => {
             expect(moveRobot("GRGRGRG")).to.equal(true);
+        })
+    })
+    describe('PaliSum', () => {
+        it('returns an array of twenty-five integers where the number plus its inverse equals a palindrome greater than 1,000', () => {
+            const arrayONumbers = paliSum();
+            expect(arrayONumbers).to.deep.equal([
+                209,  308,  407,  506,  605,
+                704,  803,  902, 1000, 1001,
+               1002, 1003, 1004, 1005, 1006,
+               1007, 1008, 1010, 1011, 1012,
+               1013, 1014, 1015, 1016, 1017
+             ]);
         })
     })
 })
