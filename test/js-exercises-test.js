6604d6a2a63c4f5a916c16e5a3e9c510208096ff
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-08-20 14:49:58 -0500

    Add collatz prompt solution and tests

--- test/js-exercises-test.js
+++ test/js-exercises-test.js
@@ -1,17 +1,17 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const { moveRobot, paliSum, factorize } = require('../javascript/index')
+const { moveRobot, paliSum, factorize, steps } = require('../javascript/index')
 
 describe('JavaScript Exercises', () => {
     describe('MoveRobot', () => {
         it('doesn\'t end at starting position', () => {
             expect(moveRobot("GRGL")).to.equal(false);
-        })
+        });
         it('ends at starting position', () => {
             expect(moveRobot("GRGRGRG")).to.equal(true);
-        })
-    })
+        });
+    });
     describe('PaliSum', () => {
         it('returns an array of twenty-five integers where the number plus its inverse equals a palindrome greater than 1,000', () => {
             const arrayONumbers = paliSum();
@@ -22,17 +22,41 @@ describe('JavaScript Exercises', () => {
                1007, 1008, 1010, 1011, 1012,
                1013, 1014, 1015, 1016, 1017
              ]);
-        })
-    })
+        });
+    });
     describe('Factorize', () => {
         it('returns an array of factors for a provided whole number', () => {
             expect(factorize(12)).to.deep.equal([1, 2, 3, 4, 6, 12]);
-        })
+        });
         it('returns an array of factors for another provided whole number', () => {
             expect(factorize(4)).to.deep.equal([1, 2, 4]);
-        })
+        });
         it('returns an array of factors for a prime number', () => {
             expect(factorize(17)).to.deep.equal([1, 17]);
-        })
-    })
+        });
+    });
+    describe('Steps', () => {
+        it('zero steps for one', () => {
+          expect(steps(1)).to.equal(0);
+        });
+        it('divide if even', () => {
+          expect(steps(16)).to.equal(4);
+        });
+        it('even and odd steps', () => {
+          expect(steps(12)).to.equal(9);
+        });
+        it('large number of even and odd steps', () => {
+          expect(steps(1000000)).to.equal(152);
+        });
+        it('zero is an error', () => {
+          expect(() => {
+            steps(0);
+          }).to.throw('Only positive numbers are allowed');
+        });
+        it('negative value is an error', () => {
+          expect(() => {
+            steps(-15);
+          }).to.throw('Only positive numbers are allowed');
+        });
+      });
 })
