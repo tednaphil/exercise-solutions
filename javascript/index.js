773191fe2a37c5c9cae2d4fe6d2a07c1fb1d06a9
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-08-21 21:19:58 -0500

    Collapse prompt text

--- javascript/index.js
+++ javascript/index.js
@@ -3,9 +3,9 @@
 //The robot moves on a plane, and its movements are described by a command string consisting of one or more
 //of the following letters:
 
-    // G instructs the robot to move forward one step
-    // L instructs the robot to turn left
-    // R instructs the robot to turn right
+// G instructs the robot to move forward one step
+// L instructs the robot to turn left
+// R instructs the robot to turn right
 
 //The robot CANNOT go backwards - poor robot.
 //After running all of the movement commands, you want to know if the robot returns to its original starting location.
@@ -24,47 +24,49 @@
 //if we receive R and current direction is north, currentDirection will be reassigned to 'e' -> so on for rest of directions
 //if we receive an L and current direction is north, current Direction will be reassigned to 'w' -> so on for the rest of directions
 
-function moveRobot (directionString) {
-    const startingPosition = [0, 0];
-    let currentPosition = [0, 0];
-    let currentDirection = 'n';
-  
-  
-    const directions = directionString.toLowerCase().split('');
-    directions.forEach(dir => {
-      if(dir === 'g' && currentDirection === 'n') {
-        currentPosition[1] += 1
-      }
-      if(dir === 'g' && currentDirection === 's') {
-        currentPosition[1] -= 1
-      }
-      if(dir === 'g' && currentDirection === 'e') {
-        currentPosition[0] += 1
-      }
-      if(dir === 'g' && currentDirection === 'w') {
-        currentPosition[0] -= 1
-      }
-      if(dir === 'r' && currentDirection === 'n') {
-        currentDirection = 'e'
-      } else if( dir === 'r' && currentDirection === 'e') {
-        currentDirection = 's'
-      } else if( dir === 'r' && currentDirection === 's') {
-        currentDirection = 'w'
-      } else if( dir === 'r' && currentDirection === 'w') {
-        currentDirection = 'n'
-      }
-      if(dir === 'l' && currentDirection === 'n') {
-        currentDirection = 'w'
-      } else if( dir === 'l' && currentDirection === 'w') {
-        currentDirection = 's'
-      } else if( dir === 'l' && currentDirection === 's') {
-        currentDirection = 'e'
-      } else if( dir === 'l' && currentDirection === 'e') {
-        currentDirection = 'n'
-      }
-    })
-    return (currentPosition[0] === startingPosition[0] && currentPosition[1] === startingPosition[1])
-  }
+function moveRobot(directionString) {
+  const startingPosition = [0, 0];
+  let currentPosition = [0, 0];
+  let currentDirection = "n";
+
+  const directions = directionString.toLowerCase().split("");
+  directions.forEach((dir) => {
+    if (dir === "g" && currentDirection === "n") {
+      currentPosition[1] += 1;
+    }
+    if (dir === "g" && currentDirection === "s") {
+      currentPosition[1] -= 1;
+    }
+    if (dir === "g" && currentDirection === "e") {
+      currentPosition[0] += 1;
+    }
+    if (dir === "g" && currentDirection === "w") {
+      currentPosition[0] -= 1;
+    }
+    if (dir === "r" && currentDirection === "n") {
+      currentDirection = "e";
+    } else if (dir === "r" && currentDirection === "e") {
+      currentDirection = "s";
+    } else if (dir === "r" && currentDirection === "s") {
+      currentDirection = "w";
+    } else if (dir === "r" && currentDirection === "w") {
+      currentDirection = "n";
+    }
+    if (dir === "l" && currentDirection === "n") {
+      currentDirection = "w";
+    } else if (dir === "l" && currentDirection === "w") {
+      currentDirection = "s";
+    } else if (dir === "l" && currentDirection === "s") {
+      currentDirection = "e";
+    } else if (dir === "l" && currentDirection === "e") {
+      currentDirection = "n";
+    }
+  });
+  return (
+    currentPosition[0] === startingPosition[0] &&
+    currentPosition[1] === startingPosition[1]
+  );
+};
 
 // PALISUM PROMPT
 // Write a method/function that starts at 0 and finds the first twenty-five numbers where the number plus its inverse equals a palindrome that is greater than 1,000.
@@ -95,7 +97,7 @@ function paliSum(num = 0, arr = []) {
   let startingNum = num;
   const paliNums = arr;
   let reverseNum = parseInt(
-    startingNum.toString().split("").reverse().join(""),
+    startingNum.toString().split("").reverse().join("")
   );
   let sum = startingNum + reverseNum;
   let isPalindrome =
@@ -109,19 +111,18 @@ function paliSum(num = 0, arr = []) {
   } else {
     return paliSum((startingNum += 1), paliNums);
   }
-}
+};
 
 //FACTORIZE PROMPT
+
 // Create a function that takes a number as its argument and returns an array of all its factors.
 
-
 // NOTES
-// - A factor is a number that evenly divides into another number without leaving a remainder. 
-// - Assume the input number will be positive. 
+// - A factor is a number that evenly divides into another number without leaving a remainder.
+// - Assume the input number will be positive.
 // - Try to implement this both iteratively & recursively if you have time.
 
-
-//PSEUDOCODE
+//PSEUDOCODE:
 //add a divider argument that will increment up with each invokation
 //define an array to collect factors
 //if num divided by divider has no remainder (!%)? push the divider to the array
@@ -129,29 +130,28 @@ function paliSum(num = 0, arr = []) {
 
 //if divider > num, return array of factors
 
+// ITERATIVE SOLUTION:
+// function factorize(num) {
+//   const factors = [];
+//   for (let i = 0; i <= num; i++){
+//     if(num % i === 0){
+//       factors.push(i)
+//     }
+//   };
+//   return factors
+// };
+
 // RECURSIVE SOLUTION:
 
-// function factorize(num, div = 1, factors = []) {
-//   if(!(num % div) && div <= num) {
-//     factors.push(div);
-//     return factorize(num, div + 1, factors)
-//   } else if(num % div && div <= num) {
-//     return factorize(num, div + 1, factors)
-//   } else if(div > num) {
-//     return factors
-//   }
-// }
-
-// ITERATIVE SOLUTION:
-
-function factorize(num) {
-  const factors = [];
-  for (let i = 0; i <= num; i++){
-    if(num % i === 0){
-      factors.push(i)
-    }
-  }; 
-  return factors
+function factorize(num, div = 1, factors = []) {
+  if (!(num % div) && div <= num) {
+    factors.push(div);
+    return factorize(num, div + 1, factors);
+  } else if (num % div && div <= num) {
+    return factorize(num, div + 1, factors);
+  } else if (div > num) {
+    return factors;
+  }
 };
 
 //STEPS COLLATZ PROMPT
@@ -181,27 +181,26 @@ function factorize(num) {
 
 // If n is not a positive integer, stop the program from being executed further and return an error message.
 
-
 //PSEUDOCODE:
 //input: positive integer greater than zero and incrementer/counter with default value of zero
 //output: integer
 
 //evaluate if number is even or odd
 //if num === 1, return counter
-//if not, 
-  //perform respective collatz operations and assign product to outcome variable
-  //then return invocation of function passing outcome and counter +1
+//if not,
+//perform respective collatz operations and assign product to outcome variable
+//then return invocation of function passing outcome and counter +1
 
 const steps = (num, counter = 0) => {
-  let isOdd = num % 2
-  if(num <= 0) {
-    throw new Error('Only positive numbers are allowed')
-  } else if(num === 1) {
-    return counter
-  } else if(isOdd) {
-    return steps((num * 3) + 1, counter + 1)
-  } else if(!isOdd) {
-    return steps(num / 2, counter + 1)
+  let isOdd = num % 2;
+  if (num <= 0) {
+    throw new Error("Only positive numbers are allowed");
+  } else if (num === 1) {
+    return counter;
+  } else if (isOdd) {
+    return steps(num * 3 + 1, counter + 1);
+  } else if (!isOdd) {
+    return steps(num / 2, counter + 1);
   }
 };
 
@@ -216,40 +215,39 @@ const steps = (num, counter = 0) => {
 //input: number
 //output: string
 //Loop to find factors of a number (omitting the number itself)
-  //if current index is a factor, add to factors array
+//if current index is a factor, add to factors array
 //define a variable to store the aliquot sum
 //iterate through the factors array returned
-  //increment the aliquotSum variable by adding each element of the factors array
+//increment the aliquotSum variable by adding each element of the factors array
 //if the aliquot sum is greater than the num, return "abundant"
 //if less than, "deficient"
 //if equal to, "perfect"
 
 const classify = (num) => {
-  if(num <= 0) {
-    throw new Error('Classification is only possible for natural numbers.')
-  };
+  if (num <= 0) {
+    throw new Error("Classification is only possible for natural numbers.");
+  }
   const factors = [];
   for (let i = 0; i < num; i++) {
-      if(num % i === 0) {
-        factors.push(i)
-      }
-  };
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
   let aSum = 0;
-  factors.forEach(factor => aSum += factor);
-  if(aSum > num) {
-    return 'abundant'
-  } else if(aSum < num) {
-    return 'deficient'
-  } else if(aSum === num) {
-    return 'perfect'
-  };
+  factors.forEach((factor) => (aSum += factor));
+  if (aSum > num) {
+    return "abundant";
+  } else if (aSum < num) {
+    return "deficient";
+  } else if (aSum === num) {
+    return "perfect";
+  }
 };
-
 
 module.exports = {
   moveRobot,
   paliSum,
   factorize,
   steps,
-  classify
+  classify,
 };
